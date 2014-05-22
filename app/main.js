29f00ec05ad6933cad92e325c5b67a94ba71d752
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-05-22 15:12:57 +0200

    Added polyfill for Function.prototype.bind

--- app/main.js
+++ app/main.js
@@ -1,3 +1,26 @@
+/********************************* Function.prototype.bind polyfill for phantomjs ********************************/
+/* */
+/* */	if (!Function.prototype.bind) {
+/* */		Function.prototype.bind = function (oThis) {
+/* */			if (typeof this !== "function") {
+/* */				throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+/* */			}
+/* */			var aArgs = Array.prototype.slice.call(arguments, 1);
+/* */			var fToBind = this;
+/* */			var fNOP = function () {};
+/* */			var fBound = function () {
+/* */				var target = this instanceof fNOP && oThis ? this : oThis;
+/* */				var args = aArgs.concat(Array.prototype.slice.call(arguments));
+/* */				return fToBind.apply(target, args);
+/* */			};
+/* */			fNOP.prototype = this.prototype;
+/* */			fBound.prototype = new fNOP();
+/* */			return fBound;
+/* */		};
+/* */	}
+/* */
+/*****************************************************************************************************************/
+
 (function () {
 	var config = {
 		dev: {
