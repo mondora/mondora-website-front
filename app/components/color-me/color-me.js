7fd55b571878bf076aa6c870936c62f665c43f28
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-07-22 17:50:09 +0200

    Refactored color-me, added tests.

--- app/components/color-me/color-me.js
+++ app/components/color-me/color-me.js
@@ -1,23 +1,28 @@
 angular.module("mnd-web.components")
 
-.directive("mndColorMe", function () {
+.factory("hexToRgbaService", function () {
+	return {
+		convert: function (color, opacity) {
+			var r = parseInt(color.slice(1,3), 16);
+			var g = parseInt(color.slice(3,5), 16);
+			var b = parseInt(color.slice(5,7), 16);
+			var a = parseFloat(opacity, 10);
+			a = isNaN(a) ? 1 : a;
+			return "rgba(" + r + "," + g + "," + b + "," + a + ")";	
+		}
+	};
+})
+
+.directive("mndColorMe", ["hexToRgbaService", function (hexToRgbaService) {
 	return {
 		restrict: "A",
 		link: function ($scope, $element, $attrs) {
-			var makeColor = function (color, opacity) {
-				var r = parseInt(color.slice(1,3), 16);
-				var g = parseInt(color.slice(3,5), 16);
-				var b = parseInt(color.slice(5,7), 16);
-				var a = parseFloat(opacity, 10);
-				a = isNaN(a) ? 1 : a;
-				return "rgba(" + r + "," + g + "," + b + "," + a + ")";
-			};
 			var color = $attrs.mndColorMe;
 			if (color) {
 				if (color[0] !== "#") {
 					color = "#" + md5(color).slice(0, 6);
 				} else {
-					color = makeColor(color, $attrs.opacity);
+					color = hexToRgbaService.convert(color, $attrs.opacity);
 				}
 			} else {
 				color = "#" + md5(Math.random().toString()).slice(0, 6);
@@ -29,5 +34,5 @@ angular.module("mnd-web.components")
 			}
 		}
 	};
-});
+}]);
 
--- test/unit/components/s3-image-upload/s3-image-upload.unit.js
+++ test/unit/components/s3-image-upload/s3-image-upload.unit.js
@@ -4,11 +4,9 @@ describe("The s3-image-upload component", function() {
 	beforeEach(module("mnd-web.components"));
 	beforeEach(module("angularFileUpload"));
 
-	var el;
-	var $scope;
-	beforeEach(inject(function ($rootScope, $compile) {
+	it("should add a hidden input element to the element to which is attched", inject(function ($rootScope, $compile) {
 		var tpl = '<i mnd-s3-image-upload before-upload="before" after-upload="after" is-uploading="isUploading" upload-progress="uploadProgress"></i>';
-		$scope = $rootScope.$new();
+		var $scope = $rootScope.$new();
 		$rootScope.Configurations = {
 			reactiveQuery: function () {
 				var result = [{
@@ -22,22 +20,13 @@ describe("The s3-image-upload component", function() {
 		};
 		$scope.before = sinon.spy();
 		$scope.after = sinon.spy();
-		el = $compile(tpl)($scope);
+		$scope.uploadProgress = 0;
+		var el = $compile(tpl)($scope);
+		console.log(el);
+
+		var input = el[0].querySelector("input");
+		input.tagName.should.equal("INPUT");
+		angular.element(input).hasClass("hidden").should.equal(true);
 	}));
-
-	it("should add a hidden input element to the element to which is attched", function () {
-		var input = el.find("input");
-		input[0].tagName.should.equal("INPUT");
-		input.hasClass("hidden").should.equal(true);
-	});
-
-	it("should trigger a click event on an hidden file input when the decorated element is clicked", function () {
-		/* TODO with protractor
-		var input = el.find("input");
-		input.click = sinon.spy();
-		el[0].click();
-		input.click.called.should.equal(true);
-		*/
-	});
 
 });
