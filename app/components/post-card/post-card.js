29caa320c7fec2ffd1ec85b7c0959fd2655e2ad3
gabriel <gabriel@mondora.com>
2014-07-23 15:02:51 +0200

    draft post and beginning of notifications style

--- app/components/post-card/post-card.js
+++ app/components/post-card/post-card.js
@@ -6,6 +6,15 @@ angular.module("mnd-web.components")
 		templateUrl: "components/post-card/post-card.html",
 		link: function ($scope, $element, $attrs) {
 			$scope.type = $attrs.type;
+
+			$scope.getTitle = function () {
+				var title;
+				if ($scope.post.title) {
+					title = $scope.post.title.trim();
+				}
+				return title || "Untitled post";
+			};
+
 		}
 	};
 });
