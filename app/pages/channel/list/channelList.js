16cb82e8f9414394fb3125d2a90332b7fa492fbe
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-08-04 12:01:53 +0200

    Added defaults to channelList search

--- app/pages/channel/list/channelList.js
+++ app/pages/channel/list/channelList.js
@@ -5,25 +5,23 @@ angular.module("mnd-web.pages")
 	var channelsRQ = $scope.Channels.reactiveQuery({published: true});
 	channelsRQ.on("change", function () {
 		$scope.safeApply(function () {
-			$scope.allChannels = channelsRQ.result;
+			$scope.channels = channelsRQ.result;
 		});
 	});
-	$scope.allChannels = channelsRQ.result;
+	$scope.channels = channelsRQ.result;
 
-	$scope.search = function (key) {
-		if (!key) {
-			$scope.safeApply(function () {
-				$scope.channels = [];
-			});
-			return;
-		}
-		Ceres.subscribe("channelsByFuzzyName", key).ready.then(function () {
-			$scope.safeApply(function () {
-				$scope.channels = $scope.allChannels.filter(function (channel) {
-					return (new RegExp(key, "i")).test(channel.name) && channel.published;
-				});
-			});
-		});
+	$scope.key = "";
+	$scope.limit = 10;
+
+	$scope.search = function () {
+		Ceres.subscribe("channelsByFuzzyName", $scope.key, $scope.limit);
 	};
+
+	$scope.loadMore = function () {
+		$scope.limit += 10;
+		$scope.search();
+	};
+
+	$scope.search();
 
 }]);
