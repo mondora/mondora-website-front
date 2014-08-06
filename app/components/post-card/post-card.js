2e06cfd49ad79e96d7141046644921362cf83f4b
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-08-06 12:05:35 +0200

    Various changes:
    Added tooltip for bookmarking.
    Moved bookmarking logic to the server.
    Small refactor of gic-image component.

--- app/components/post-card/post-card.js
+++ app/components/post-card/post-card.js
@@ -37,46 +37,23 @@ angular.module("mnd-web.components")
 			///////////////
 
 			var Tasks = Ceres.getCollection("tasks");
-			var DEFAULT_POMODORO_DURATION = 25 * 60 * 1000;
 			$scope.bookmark = function () {
-				var pr = Tasks.insert({
-					userId: $scope.user._id,
-					addedBy: {
-						userId: $scope.user._id,
-						name: $scope.user.profile.name,
-						screenName: $scope.user.profile.screenName,
-						pictureUrl: $scope.user.profile.pictureUrl
-					},
-					participants: [{
-						userId: $scope.user._id,
-						name: $scope.user.profile.name,
-						screenName: $scope.user.profile.screenName,
-						pictureUrl: $scope.user.profile.pictureUrl
-					}],
-					pomodoros: [{
-						_id: $scope.guid(),
-						events: [],
-						status: "pristine",
-						duration: DEFAULT_POMODORO_DURATION
-					}],
-					date: Date.now(),
-					status: "todo",
-					name: $scope.post.title,
-					details: {
-						post: {
-							_id: $scope.post._id,
-							title: $scope.post.title,
-							subtitle: $scope.post.subtitle,
-							author: $scope.post.authors[0]
-						}
-					},
-					tags: ["bookmark"]
-				});
+				Ceres.call("bookmarkPost", $scope.post._id);
 			};
 
 			$scope.userBookmarkedPost = function () {
 				var bookmarksByPost = Tasks.reactiveQuery({"details.post._id": $scope.post._id}).result;
 				return bookmarksByPost.length > 0;
+			};
+
+			$scope.bookmarkHtml = function () {
+				var html;
+				if ($scope.userBookmarkedPost()) {
+					html = '<span class="mnd-width-80">Added to your bookmarks</span>';
+				} else {
+					html = '<span class="mnd-width-80">Bookmark this post</span>';
+				}
+				return html;
 			};
 
 		}
