8e008f48a6726865e48c5eac9ac7e2739be2fb13
gabriel <gabriel@mondora.com>
2014-08-07 11:04:23 +0200

    styled channel list

--- app/pages/channel/list/channelList.js
+++ app/pages/channel/list/channelList.js
@@ -24,4 +24,7 @@ angular.module("mnd-web.pages")
 
 	$scope.search();
 
+	//immagine moccata
+	$scope.channelListImageUrl = "https://s3-eu-west-1.amazonaws.com/ngtest/9ec1a6f4ec3b381c1f3fb971df0a63c9";
+
 }]);
