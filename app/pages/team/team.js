186eaa78303d1447aa768ca879a9ca4e1b803bd6
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-09-04 11:32:32 +0200

    Various changes:
    - removed the mondoraTeamMember property
      from the team page
    - added SEO tags for user pages

--- app/pages/team/team.js
+++ app/pages/team/team.js
@@ -2,7 +2,9 @@ angular.module("mnd-web.pages")
 
 .controller("TeamController", ["$scope", function ($scope) {
 
-	var teamQuery = $scope.Users.reactiveQuery({mondoraTeamMember: true});
+	var teamQuery = $scope.Users.reactiveQuery(function (user) {
+		return _.contains(user.roles, "mondora");
+	});
 	teamQuery.on("change", function () {
 		$scope.safeApply(function () {
 			$scope.team = teamQuery.result;
@@ -10,11 +12,14 @@ angular.module("mnd-web.pages")
 	});
 	$scope.team = teamQuery.result;
 
+	$scope.isTeamMember = function () {
+		return $scope.user && _.contains($scope.user.roles, "mondora");
+	};
+
 	$scope.sendLinkedInProfile = function (url) {
 		$scope.Ceres.call("sendLinkedInProfileUrl", url);
 		$scope.linkSent = true;
 	};
-
 	$scope.checkLink = function (url) {
 		return /linkedin\.com/.test(url);
 	};
