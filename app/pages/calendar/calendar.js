6e114d84a06956f6ac1df081fd6e2fe4628b4ca2
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-09-26 15:16:24 +0200

    Various changes:
    - added possibility to freeze and unfreeze coins
    - restricted access to the UI for managing coins to coin managers

--- app/pages/calendar/calendar.js
+++ app/pages/calendar/calendar.js
@@ -1,16 +1,24 @@
 angular.module("mnd-web.pages")
 
 .controller("CoinModalController", ["$scope", function ($scope) {
-	$scope.day = $scope.selectedDay.day;
+	var Coins = Ceres.getCollection("coins");
+	var day = $scope.selectedDay.day;
 	$scope.printDay = function () {
-		return $scope.day.moment.format("dddd, MMMM Do YYYY");
+		return day.moment.format("dddd, MMMM Do YYYY");
 	};
-	if ($scope.day.coin) {
-		$scope.coin = $scope.day.coin;
+	if (day.coin) {
 		$scope.isUpdate = true;
+		var coinRQ = Coins.reactiveQuery({_id: day.coin._id});
+		coinRQ.on("change", function () {
+			$scope.safeApply(updateCoin);
+		});
+		var updateCoin = function () {
+			$scope.coin = coinRQ.result[0];
+		};
+		updateCoin();
 	} else {
 		$scope.coin = {
-			day: $scope.day.moment.valueOf(),
+			day: day.moment.valueOf(),
 			activities: [{
 				// XXX prefill with the latest activity
 			}]
@@ -21,14 +29,24 @@ angular.module("mnd-web.pages")
 		$scope.modalStatus.day = false;
 	};
 	$scope.updateCoin = function () {
-		Ceres.getCollection("coins").update($scope.coin._id, {
+		Coins.update($scope.coin._id, {
 			activities: angular.copy($scope.coin.activities)
 		});
 		$scope.modalStatus.day = false;
 	};
 	$scope.removeCoin = function () {
-		Ceres.getCollection("coins").remove($scope.coin._id);
+		Coins.remove($scope.coin._id);
 		$scope.modalStatus.day = false;
+	};
+	$scope.freezeCoin = function () {
+		Coins.update($scope.coin._id, {
+			frozen: true
+		});
+	};
+	$scope.unfreezeCoin = function () {
+		Coins.update($scope.coin._id, {
+			frozen: false
+		});
 	};
 }])
 
