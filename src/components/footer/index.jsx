aae962ff3ea9d0b583143b782180dc7fe68d61d1
Filippo scarry <49690013+ScarryP@users.noreply.github.com>
2019-07-15 12:39:13 +0200

    chore: add social network icons in footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,6 +4,12 @@ import styled from "styled-components";
 
 import StackPanel from "../stackpanel";
 
+import twitterLogo from "../../../static/images/twitter.png"
+import instagramLogo from "../../../static/images/instagram.png"
+import facebookLogo from "../../../static/images/facebook.png"
+import linkedinLogo from "../../../static/images/linkedin.png"
+import pinterestLogo from "../../../static/images/pinterest.png"
+
 const Container = styled.div`
     padding: 32px 192px;
     font-size: ${props => props.theme.size.text.subtle};
@@ -24,9 +30,13 @@ const Container = styled.div`
 `;
 
 const MiniLogo = styled.div`
+    &:before{
+        color: #ffea00;
+        content: ":";
+    }
     margin-top: -10px;
     grid-area: 1 / 1 / 3 / 2;
-    font-size: ${props => props.theme.size.text.mega};
+    font-size: ${props => props.theme.size.text.megaM};
     /* background-color: rebeccapurple; */
 
     @media (max-width: 992px) {
@@ -60,10 +70,48 @@ const Misc = styled.div`
         grid-area: 3 / 1 / 4 / 3;
     }
 `;
+const Menuitem = styled.div`
+    display: block;
+    margin: 0;
+    width: 20%;
+    padding: 5px;
+`;
+const SocialLogo = styled.img`
+    display: block;
+    margin: 0;
+    width: 50px;
+`;
+const links = [
+    {
+        to: "https://www.instagram.com/mondoracom/",
+        text: "Instagram",
+        logo: instagramLogo
+    },
+    {
+        to: "https://it-it.facebook.com/mondoracom/",
+        text: "Facebook",
+        logo: facebookLogo
+    },
+    {
+        to: "https://www.linkedin.com/company/mondora-s-p-a-/?originalSubdomain=it",
+        text: "Linkedin",
+        logo: linkedinLogo
+    },
+    {
+        to: "https://twitter.com/mondora",
+        text: "Twitter",
+        logo: twitterLogo
+    },
+    {
+        to: "https://www.pinterest.it/mondoracom/",
+        text: "Pinterest",
+        logo: pinterestLogo
+    },
+];
 
 const Footer = () => (
     <Container justify="center">
-        <MiniLogo>{":m"}</MiniLogo>
+        <MiniLogo>{"m"}</MiniLogo>
 
         <Info>
             <StackPanel direction="column">
@@ -94,7 +142,15 @@ const Footer = () => (
                     <p>{`+39 0342 1856456 - info@mondora.com`}</p>
                 </span>
                 <span>
-                    <p>{`socials`}</p>
+                        
+                    <StackPanel gutter={16}>
+                        {links.map(link => (
+                            <Menuitem>
+                                 <a href={link.to}><SocialLogo src={link.logo}/></a>
+                                
+                            </Menuitem>
+                        ))}
+                    </StackPanel>
                 </span>
             </StackPanel>
         </Misc>
