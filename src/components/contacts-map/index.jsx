01d710bd7f098b74a61c74c01d4444b9e5023512
Filippo scarry <49690013+ScarryP@users.noreply.github.com>
2019-09-13 10:42:53 +0200

    chore: update map style

--- src/components/contacts-map/index.jsx
+++ src/components/contacts-map/index.jsx
@@ -15,16 +15,126 @@ const PointerImg = styled.img`
     left: -56px;
     border-radius: 100%;
 `;
+const MapContainer = styled.div`
+    height: 520px;
+    width: 100%;
+`;
+
+function createMapOptions(maps) {
+    return {
+        zoomControlOptions: {
+            position: maps.ControlPosition.RIGHT_CENTER,
+            style: maps.ZoomControlStyle.SMALL
+        },
+        styles: [
+            { elementType: "geometry", stylers: [{ color: "#ededee" }] },
+            {
+                elementType: "labels.text.stroke",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#9f9f9f" }]
+            },
+            {
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#9f9f9f" }]
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.icon",
+                stylers: [{ saturation: "-100" }]
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{ color: "#c8c8c8" }]
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{ color: "#ffffff" }]
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#e5e5e5" }]
+            },
+            {
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#636363" }]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{ color: "#c8c8c8" }]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#c9c9c9" }]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                featureType: "road",
+                elementType: "labels.icon",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{ color: "#2f3948" }]
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#a6a6a6" }]
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.icon",
+                stylers: [{ saturation: "-100" }]
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{ color: "#c8c8c8" }]
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#5c5c5c" }]
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{ visibility: "off" }]
+            }
+        ]
+    };
+}
 
 const ContactsMap = ({ position, zoom }) => {
     return (
-        <div style={{ height: "520px", width: "100%" }}>
+        <MapContainer>
             <GoogleMapReact
                 bootstrapURLKeys={{
                     key: process.env.GATSBY_MAPS_API_KEY
                 }}
                 defaultCenter={position}
                 defaultZoom={zoom}
+                options={createMapOptions}
             >
                 <PointerImg
                     lat={position.lat}
@@ -32,7 +142,7 @@ const ContactsMap = ({ position, zoom }) => {
                     src={PointerSrc}
                 />
             </GoogleMapReact>
-        </div>
+        </MapContainer>
     );
 };
 
