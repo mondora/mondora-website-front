f69aed3011bab8aa681cd1cbe086ca876e409d13
luzzif <federico.luzzi@mondora.com>
2019-11-04 21:19:10 +0100

    refactor: refactor mobile menu with added animations

---
+++ src/components/layout/menu/mobile/styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Grid from "../../../grid";
+import { Link } from "gatsby";
+import { theme } from "../../../../styles/theme";
+
+const toolbarHeight = theme.spacing.unit * 18;
+
+export const ToolbarGrid = styled(Grid)`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: var(--background-dark-gray);
+    height: ${toolbarHeight}px;
+    padding: 0 ${props => props.theme.spacing.unit * 4}px;
+    z-index: 24;
+    border-bottom: 2px solid #fff;
+`;
+
+export const Icon = styled(FontAwesomeIcon)`
+    font-size: ${props => props.theme.spacing.unit * 4}pt;
+    color: #fff;
+`;
+
+export const MenuItem = styled(Grid)`
+    height: ${props => props.theme.spacing.unit * 12}px;
+    text-decoration: none;
+    border-bottom: 1px solid var(--border-dark-gray);
+    padding: 0 ${props => props.theme.spacing.unit * 4}px;
+    box-sizing: border-box;
+`;
+
+export const AnimatedMenu = styled(Grid)`
+    padding-top: ${toolbarHeight}px;
+    background: var(--background-dark-gray);
+    position: fixed;
+    right: 0;
+    left: 0;
+    top: ${props => (props.open ? "0" : "-100%")};
+    transition: top 0.3s ease-in-out;
+`;
+
+export const MenuLink = styled(Link)`
+    text-decoration: none;
+    color: var(--white);
+`;
+
+export const BlogLink = styled.div`
+    text-decoration: none;
+    color: var(--white);
+`;
+
+export const Spacer = styled.div`
+    height: ${toolbarHeight}px;
+`;
