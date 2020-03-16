4a4dc30ce34483e76605d1a685d9c87a753a2291
Manuel Serra <nitroin@hotmail.com>
2020-03-16 11:48:47 +0100

    style: background coriandoli 🎉

--- src/components/background-stripe/index.js
+++ src/components/background-stripe/index.js
@@ -10,7 +10,7 @@ const BackgroundStripe = styled.div`
     padding: 32px 0;
 
     background-position: center;
-    background-size: contain;
+    background-size: 584px;
 
     ${props =>
         props.theme === darkTheme &&
