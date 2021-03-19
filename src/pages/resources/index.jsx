98ec8b0909de6a620dadb79b63fa357b62f7d348
ScarryP <49690013+ScarryP@users.noreply.github.com>
2021-03-19 12:37:40 +0100

    feat: basic resource filtering based on tags

--- src/pages/resources/index.jsx
+++ src/pages/resources/index.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { graphql, useStaticQuery } from "gatsby";
+
+import { Box } from "reflexbox";
+import { Label, Checkbox } from "@rebass/forms";
 
 import Header from "../../components/header";
 import Layout from "../../components/layout";
 import PageMetadata from "../../components/page-metadata";
 import MaxWidthContainer from "../../components/max-width-container";
 import Resource from "../../components/resource";
+import BackgroundStripe from "../../components/background-stripe";
+import Title from "../../components/title";
 
 const Resources = () => {
-    const { contentfulResourcesPage, allContentfulResource } = useStaticQuery(graphql`
+    const [tags, setTags] = useState({});
+    console.log(tags);
+
+    const filterResource = resource => {
+        if (
+            Object.entries(tags).find(tag =>
+                tag[1] && resource.tags.includes(tag[0]) ? 1 : 0
+            ) ||
+            !Object.values(tags).find(value => value)
+        )
+            return resource;
+    };
+
+    const {
+        contentfulResourcesPage,
+        allContentfulResource
+    } = useStaticQuery(graphql`
         query {
             contentfulResourcesPage {
                 metaDescr {
@@ -35,21 +56,29 @@ const Resources = () => {
                 }
             }
             allContentfulResource {
-                nodes {
-                  title
-                  type
-                  date
-                  button {
-                    link
-                    text
-                  }
-                  areas
-                  tags
-                  description {
-                    description
-                  }
+                group(field: tags) {
+                    totalCount
+                    fieldValue
                 }
-              }
+                group(field: tags) {
+                    totalCount
+                    fieldValue
+                }
+                nodes {
+                    title
+                    type
+                    date
+                    button {
+                        link
+                        text
+                    }
+                    areas
+                    tags
+                    description {
+                        description
+                    }
+                }
+            }
         }
     `);
 
@@ -61,15 +90,41 @@ const Resources = () => {
             />
             <Header
                 left={
-                    contentfulResourcesPage.leftHeader.childMarkdownRemark.htmlAst
+                    contentfulResourcesPage.leftHeader.childMarkdownRemark
+                        .htmlAst
                 }
                 rightImage={contentfulResourcesPage.rightImage}
             />
             <MaxWidthContainer>
-                {
-                allContentfulResource.nodes.map((resouce, i) => (
-                    <Resource data={resouce} key={i}/>
-                ))}
+                <BackgroundStripe theme="light">
+                    <Box as="form" onSubmit={e => e.preventDefault()}>
+                        <Title>{"Tags"}</Title>
+                        {allContentfulResource.group.map((tag, i) => (
+                            <Label key={i}>
+                                <Checkbox
+                                    id={tag.fieldValue}
+                                    name={tag.fieldValue}
+                                    onChange={event =>
+                                        setTags({
+                                            ...tags,
+                                            [tag.fieldValue]:
+                                                event.target.checked
+                                        })
+                                    }
+                                />
+                                {tag.fieldValue}
+                                {" - ( "}
+                                {tag.totalCount}
+                                {" ) "}
+                            </Label>
+                        ))}
+                    </Box>
+                </BackgroundStripe>
+                {allContentfulResource.nodes
+                    .filter(resource => filterResource(resource))
+                    .map((resouce, i) => (
+                        <Resource data={resouce} key={i} />
+                    ))}
             </MaxWidthContainer>
         </Layout>
     );
